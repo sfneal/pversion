adc5aae8db332f91f8726ef66ed9085f7b29d6d4
mrstephenneal <stephen.neal14@gmail.com>
2021-01-19 15:07:52 -0500

    FIX get_version function
    MAKE requirements.txt.

--- pversion/pversion.py
+++ pversion/pversion.py
@@ -9,7 +9,7 @@ def get_version(package_name, version_file='_version.py'):
     :param version_file: version file name (inside package root)
     :return: package version
     """
-    filename = os.path.join(os.path.dirname(__file__), package_name, version_file)
+    filename = os.path.join(package_name, version_file)
     with open(filename, 'rb') as fp:
         return fp.read().decode('utf8').split('=')[1].strip(" \n'")
 
